5715b4b03ce0e53931db8521eb3db97545b77877
insub4067 <vb15@kakao.com>
2022-11-27 18:52:21 +0900

    refactor : Removed Storyboard

---
+++ POSMealUIKit/Controller/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  POSMealUIKit
+//
+//  Created by Kim Insub on 2022/11/27.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
